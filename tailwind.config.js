9255fd97ac781c384d3d29212c6a9a1e8099e065
MiaTrinity <mia.trinity.codes@gmail.com>
2020-12-21 11:15:16 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.liquid',
+    './src/**/*.md',
+    './frontend/javascript/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
